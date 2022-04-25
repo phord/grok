7b62dba71f1010227dac77855ec7bdc64c74a75d
Phil Hord <phord@purestorage.com>
2022-04-25 13:56:28 -0700

    Load index for input files on start
    Currently this panics if the file is not loaded.  Also runs very slow since indexing
    happens up-front.

--- grok/src/viewer.rs
+++ grok/src/viewer.rs
@@ -1,19 +1,23 @@
 use crate::config::Config;
 use crate::display::Display;
 use crate::keyboard::{Input, UserCommand};
+use indexed_file::indexer::LogFile;
 
 pub struct Viewer {
     config: Config,
     display: Display,
     input: Input,
+    file: LogFile,
 }
 
 impl Viewer {
     pub fn new(config: Config) -> Self {
+        let filename = config.filename.get(0).expect("No filename specified").clone();
         Self {
             config: config.clone(),
             display: Display::new(config),
             input: Input::new(),
+            file: LogFile::new(Some(filename)).expect("Failed to open file"),
         }
     }
 
