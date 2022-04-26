21637f6a6ee943fca880e1ab5ab62d4b03b7c085
Phil Hord <phord@purestorage.com>
2022-04-25 17:29:41 -0700

    Add static from_string keymap

--- grok/src/keyboard.rs
+++ grok/src/keyboard.rs
@@ -1,12 +1,19 @@
-use crossterm::event::{Event, KeyCode, KeyEvent};
-use crossterm::{cursor, event, execute, queue, terminal};
-use crossterm::terminal::ClearType;
-use std::io::stdout;
-use std::cmp;
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
+use crossterm::{event, terminal};
+use std::collections::HashMap;
 use std::time::Duration;
-use std::io::Write;
 
+use UserCommand as cmd;
+const KEYMAP: &'static [(&'static str, UserCommand)] = &[
+    ("Ctrl+Q", cmd::Quit),
+    ("Esc", cmd::Quit),
+    ("Up", cmd::ScrollUp),
+    ("Down", cmd::ScrollDown),
+    ("PageUp", cmd::PageUp),
+    ("PageDown", cmd::PageDown),
+];
 
+#[derive(Copy, Clone)]
 pub enum UserCommand {
     None,
     Quit,
@@ -32,6 +39,7 @@ impl Reader {
 pub struct Input {
     reader: Reader,
     started: bool,
+    keymap: HashMap<KeyEvent, UserCommand>,
 }
 
 impl Drop for Input {
@@ -44,10 +52,79 @@ impl Drop for Input {
 
 impl Input {
     pub fn new() -> Self {
+        // TODO:
+        let keymap: HashMap<_, _> = KEYMAP
+            .iter()
+            .map(|(key, cmd)| (Self::keycode(key).unwrap(), *cmd))
+            .collect();
+
         Self {
             reader: Reader,
             started: false,
+            keymap,
         }
+    }
+
+    /// Convert a string representation of a key combo into a KeyEvent
+    /// ```
+    /// assert_eq!(crate::Input::keycode("Ctrl+Q"), KeyEvent::new(KeyCode::Char('q'), KeyModifiers::CONTROL));
+    /// ```
+    fn keycode(orig: &str) -> Result<KeyEvent, String> {
+        let mut result = KeyEvent::new(KeyCode::Null, KeyModifiers::NONE);
+
+        let str = orig.to_lowercase();
+        for key in str.split("+") {
+            let mods = match key {
+                "shift" => crossterm::event::KeyModifiers::SHIFT,
+                "alt" => crossterm::event::KeyModifiers::ALT,
+                "ctrl" => crossterm::event::KeyModifiers::CONTROL,
+                _ => crossterm::event::KeyModifiers::NONE,
+            };
+
+            let action = match key {
+                "backspace" => KeyCode::Backspace,
+                "enter" => KeyCode::Enter,
+                "left" => KeyCode::Left,
+                "right" => KeyCode::Right,
+                "up" => KeyCode::Up,
+                "down" => KeyCode::Down,
+                "home" => KeyCode::Home,
+                "end" => KeyCode::End,
+                "pageup" => KeyCode::PageUp,
+                "pagedown" => KeyCode::PageDown,
+                "tab" => KeyCode::Tab,
+                "backtab" => KeyCode::BackTab,
+                "delete" => KeyCode::Delete,
+                "insert" => KeyCode::Insert,
+                "null" => KeyCode::Null,
+                "esc" => KeyCode::Esc,
+                k => {
+                    if k.len() == 1 {
+                        KeyCode::Char(k.chars().next().unwrap())
+                    } else if k.len() > 1 && k.starts_with("F") && k.len() < 4 {
+                        KeyCode::F(k[1..].parse().unwrap())
+                    } else {
+                        KeyCode::Null
+                    }
+                }
+            };
+
+            if mods != KeyModifiers::NONE {
+                if result.modifiers & mods != KeyModifiers::NONE {
+                    return Err(format!("Key combo {} gives {} twice", orig, key));
+                }
+                result.modifiers |= mods;
+            } else if action != KeyCode::Null {
+                // Already got an action key
+                if result.code != KeyCode::Null {
+                    return Err(format!("Key combo {} has two action keys", orig));
+                }
+                result.code = action;
+            } else {
+                return Err(format!("Unknown key name {} in {}", key, orig));
+            }
+        }
+        Ok(result)
     }
 
     fn start(&mut self) -> crossterm::Result<()> {
@@ -67,32 +144,11 @@ impl Input {
     }
 
     pub fn process_keypress(&mut self) -> crossterm::Result<UserCommand> {
-        match self.read_key()? {
-            KeyEvent {
-                code: KeyCode::Char('q'),
-                modifiers: event::KeyModifiers::CONTROL,
-            } => return Ok(UserCommand::Quit),
-            KeyEvent {
-                code: KeyCode::Esc,
-                modifiers: event::KeyModifiers::NONE,
-            } => return Ok(UserCommand::Quit),
-            // KeyEvent {
-            //     code: direction @ (KeyCode::Up | KeyCode::Down | KeyCode::Left | KeyCode::Right
-            //                        | KeyCode::Home | KeyCode::End),
-            //     modifiers: event::KeyModifiers::NONE,
-            // } => self.output.move_cursor(direction),
-            // KeyEvent {
-            //     code: val @ (KeyCode::PageUp | KeyCode::PageDown),
-            //     modifiers: event::KeyModifiers::NONE,
-            // } => (0..self.output.win_size.1).for_each(|_| {
-            //     self.output.move_cursor(if matches!(val, KeyCode::PageUp) {
-            //         KeyCode::Up
-            //     } else {
-            //         KeyCode::Down
-            //     });
-            // }),
-            _ => {}
+        let event = self.read_key()?;
+        let cmd = self.keymap.get(&event);
+        match cmd {
+            Some(cmd) => Ok(*cmd),
+            None => Ok(UserCommand::None),
         }
-    Ok(UserCommand::None)
     }
 }
