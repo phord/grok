e1090313f0fbc7a21d99d2284b2651d2e381fdff
Phil Hord <phord@purestorage.com>
2023-03-27 19:26:29 -0700

    AsyncStdin: refactor and impl {Seek, Read}
    Test shows that BufReader deals with growing file over time just fine.

---
+++ play/src/async_stdin.rs
@@ -0,0 +1,105 @@
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::TryRecvError;
+use std::thread;
+
+pub struct AsyncStdin {
+    buffer: Vec<u8>,
+    rx: Option<Receiver<Vec<u8>>>,
+    pos:u64,
+}
+
+impl AsyncStdin {
+    pub fn new() -> Self {
+        let rx = Some(Self::spawn_stdin_channel());
+        Self {
+            rx,
+            buffer: Vec::default(),
+            pos: 0,
+        }
+    }
+
+    pub fn is_eof(&self) -> bool {
+        !self.rx.is_some()
+    }
+
+    pub fn len(&self) -> usize {
+        self.buffer.len()
+    }
+
+    pub fn fill_buffer(&mut self) {
+        if let Some(rx) = &self.rx {
+            loop {
+                match rx.try_recv() {
+                    Ok(mut data) => self.buffer.append(&mut data),
+                    Err(TryRecvError::Empty) => break,
+                    Err(TryRecvError::Disconnected) => {
+                        self.rx = None;
+                        break;
+                        // panic!("Channel disconnected"),
+                    },
+                }
+            }
+        }
+    }
+
+    fn spawn_stdin_channel() -> Receiver<Vec<u8>> {
+        // Use a bounded channel to prevent stdin from running away from us
+        let (tx, rx) = mpsc::sync_channel::<Vec<u8>>(100);
+        let mut buffer = String::new();
+        thread::spawn(move || loop {
+            buffer.clear();
+            // TODO: Read into a Vec<u8> and avoid utf8-validation of the data
+            // TODO: Handle data with no line-feeds
+            match std::io::stdin().read_line(&mut buffer) {
+                Ok(0) => break,  // EOF
+                Ok(_) => tx.send(buffer.as_bytes().iter().copied().collect()).unwrap(),
+                Err(err) => { eprint!("{:?}", err); break; },
+            }
+        });
+        rx
+    }
+}
+
+use std::io::Read;
+impl Read for AsyncStdin {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        self.fill_buffer();
+        let start = self.pos as usize;
+        let len = buf.len().min(self.len().saturating_sub(start));
+        if len > 0 {
+            let end = start + len;
+            buf[..len].copy_from_slice(&self.buffer[start..end]);
+            self.pos = self.pos.saturating_add(len as u64);
+        }
+        Ok(len)
+    }
+}
+
+use std::io::{Seek, SeekFrom};
+impl Seek for AsyncStdin {
+    fn seek(&mut self, pos: std::io::SeekFrom) -> std::io::Result<u64> {
+        eprint!("AsyncStdin::seek {:?}  ", pos);
+
+        let (start, offset) = match pos {
+            SeekFrom::Start(n) => {
+                (0_i64, n as i64)
+            }
+            SeekFrom::Current(n) => {
+                (self.pos as i64, n)
+            }
+            SeekFrom::End(n) => {
+                (self.len() as i64, n)
+            }
+        };
+        self.pos = (((start as i64).saturating_add(offset)) as u64).min(self.len() as u64);
+        eprintln!("  >> len={} pos={}", self.len(), self.pos);
+        Ok(self.pos)
+    }
+}
+
+// use std::io::BufRead;
+// // BufReader<AsyncStdin> is pointless As BufReader takes ownership away from us
+// impl BufRead for AsyncStdin {
+
+// }
--- play/src/main.rs
+++ play/src/main.rs
@@ -1,5 +1,8 @@
+mod async_stdin;
 
 use std::{time::{SystemTime, UNIX_EPOCH}, io::Read};
+use async_stdin::AsyncStdin;
+use std::thread::sleep;
 
 fn millis() -> u64 {
     let start = SystemTime::now();
@@ -75,8 +78,8 @@ fn cat() {
   io::copy(&mut rdr, &mut io::stdout()).unwrap();
 }
 
-fn is_seekable(file: &mut std::fs::File) -> bool {
-    use std::io::{Seek, SeekFrom::Current};
+fn is_seekable(file: &mut dyn std::io::Seek) -> bool {
+    use std::io::{SeekFrom::Current};
     file.seek(Current(0)).is_ok()
 }
 
@@ -117,71 +120,6 @@ fn stdin_seek_front_to_back() -> std::io::Result<()>{
 }
 
 
-// From https://stackoverflow.com/questions/30012995/how-can-i-read-non-blocking-from-stdin
-
-
-use std::sync::mpsc;
-use std::sync::mpsc::Receiver;
-use std::sync::mpsc::TryRecvError;
-use std::thread;
-use std::thread::sleep;
-
-struct AsyncStdin {
-    buffer: Vec<u8>,
-    rx: Option<Receiver<Vec<u8>>>,
-}
-
-impl AsyncStdin {
-    pub fn new() -> Self {
-        let rx = Some(Self::spawn_stdin_channel());
-        Self {
-            rx,
-            buffer: Vec::default(),
-        }
-    }
-
-    fn is_eof(&self) -> bool {
-        !self.rx.is_some()
-    }
-
-    fn len(&self) -> usize {
-        self.buffer.len()
-    }
-
-    fn fill_buffer(&mut self) {
-        if let Some(rx) = &self.rx {
-            loop {
-                match rx.try_recv() {
-                    Ok(mut data) => self.buffer.append(&mut data),
-                    Err(TryRecvError::Empty) => break,
-                    Err(TryRecvError::Disconnected) => {
-                        self.rx = None;
-                        break;
-                        // panic!("Channel disconnected"),
-                    },
-                }
-            }
-        }
-    }
-
-    fn spawn_stdin_channel() -> Receiver<Vec<u8>> {
-        // Use a bounded channel to prevent stdin from running away from us
-        let (tx, rx) = mpsc::sync_channel::<Vec<u8>>(100);
-        let mut buffer = String::new();
-        thread::spawn(move || loop {
-            buffer.clear();
-            // TODO: Read into a Vec<u8> and avoid utf8-validation of the data
-            // TODO: Handle data with no line-feeds
-            match std::io::stdin().read_line(&mut buffer) {
-                Ok(0) => break,  // EOF
-                Ok(_) => tx.send(buffer.as_bytes().iter().copied().collect()).unwrap(),
-                Err(err) => { eprint!("{:?}", err); break; },
-            }
-        });
-        rx
-    }
-}
-
 //  (for x in {1..100} ; do echo $x ; sleep 0.1 ; done) | cargo run --bin play
 fn try_async_stdin() {
     let mut stdin = AsyncStdin::new();
@@ -195,7 +133,11 @@ fn try_async_stdin() {
 
         let len = stdin.len();
         if prev_len != len {
-            let s = String::from_utf8(stdin.buffer[prev_len..].to_vec()).unwrap();
+            let bytes = len - prev_len;
+            let mut buf = vec![0u8; bytes];
+            let actual = stdin.read(&mut buf).unwrap();
+            assert_eq!(bytes, actual);
+            let s = String::from_utf8(buf).unwrap();
             prev_len = len;
             prev = now;
             print!("{elapsed}ms: {s}");
@@ -219,7 +161,11 @@ fn try_async_stdin_terminate_early() {
 
         let len = stdin.len();
         if prev_len != len {
-            let s = String::from_utf8(stdin.buffer[prev_len..].to_vec()).unwrap();
+            let bytes = len - prev_len;
+            let mut buf = vec![0u8; bytes];
+            let actual = stdin.read(&mut buf).unwrap();
+            assert_eq!(bytes, actual);
+            let s = String::from_utf8(buf).unwrap();
             prev_len = len;
             prev = now;
             counter += 1;
@@ -229,12 +175,37 @@ fn try_async_stdin_terminate_early() {
     }
 }
 
+// Seek works in AsyncStdin
+fn async_stdin_seek_front_to_back() -> std::io::Result<()>{
+    use std::io::{self, BufRead, BufReader, Seek, SeekFrom};
+    let mut file = AsyncStdin::new();
+
+    assert!(is_seekable(&mut file));
+
+    let mut buffer = String::new();
+    let mut handle = BufReader::new(file);
+
+    for _ in 0..4 {
+        sleep(std::time::Duration::from_millis(200));
+
+        handle.seek(SeekFrom::Start(0)).expect("file is seekable");
+        let l1 = { buffer.clear(); handle.read_line(&mut buffer).expect("read_line doesn't fail"); buffer.clone() };
+        let l2 = { buffer.clear(); handle.read_line(&mut buffer).expect("read_line doesn't fail"); buffer.clone() };
+        handle.seek(SeekFrom::End(-10)).expect("file is seekable");
+        let _dummy = { buffer.clear(); handle.read_line(&mut buffer).expect("read_line doesn't fail"); buffer.clone() };
+        let l100 = { buffer.clear(); handle.read_line(&mut buffer).expect("read_line doesn't fail"); buffer.clone() };
+        println!("l1:{l1}l2:{l2}l100:{l100}");
+    }
+    Result::Ok(())
+}
+
 
 fn main() {
     // stdin_read_one_line();
     // stdin_read_all_lines();
     // stdin_read_bytes();
     // stdin_seek_front_to_back().expect("failed");
+    async_stdin_seek_front_to_back().expect("failed");
     // try_async_stdin();
-    try_async_stdin_terminate_early();
+    // try_async_stdin_terminate_early();
 }
