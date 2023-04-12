33fc86ed07614b8b940ffd92d86dcc693e581dae
Phil Hord <phord@purestorage.com>
2023-04-11 19:10:59 -0700

    WIP: Experiment with many ways to impl a cat

--- cat/src/cat.rs
+++ cat/src/cat.rs
@@ -1,25 +1,101 @@
 use crate::config::Config;
 use indexed_file::{line_indexer::LineIndexer, files};
+use indexed_file::files::ZstdLogFile;
+use std::io::{BufRead, Write};
+use std::path::PathBuf;
 
+
+fn get_files_from_cfg() -> Vec<Option<PathBuf>> {
+    let cfg = Config::from_env().expect("Config should not error");
+    let mut files:Vec<Option<_>> = cfg.filename.iter().cloned().map(|file| Some(file)).collect();
+    if files.is_empty() {
+        files.push(None);
+    };
+    files
+}
+
+#[allow(dead_code)]
 pub fn cat_cmd() {
-    let cfg = Config::from_env();
+    for file in get_files_from_cfg() {
+        let mut file = LineIndexer::new(files::new_text_file(file).unwrap());
+        // TODO: Open all files at once and iterate them sorted if timestamped
+        // TODO: Print lines with colors
+        for (line, _start, _end) in file.iter_lines() {
+            println!("{line}");
+        }
+    }
+}
 
-    if cfg.is_ok() {
-        let cfg = cfg.unwrap();
-        let mut files:Vec<Option<_>> = cfg.filename.iter().cloned().map(|file| Some(file)).collect();
-        if files.is_empty() {
-            files.push(None);
-        };
-        for file in files {
-            // println!("{:?}", file);
+// Copy directly from our file to stdout using io::copy
+#[allow(dead_code)]
+pub fn copycat_cmd() {
+    for file in get_files_from_cfg() {
+        let mut file = files::new_text_file(file).expect("File failed to open");
+        std::io::copy(&mut file, &mut std::io::stdout()).expect("We don't need error handling");
+    }
+}
 
-            // TODO: Teach LineIndexer to open the file by names and construct the correct type
-            let mut file = LineIndexer::new( files::new_text_file(file).unwrap());
-            // TODO: Open all files at once and iterate them sorted if timestamped
-            // TODO: Print lines with colors
-            for (line, _start, _end) in file.iter_lines() {
-                println!("{line}");
+// Iterate using BufRead::lines()
+#[allow(dead_code)]
+pub fn itercat_cmd() {
+    let mut out = std::io::stdout();
+    for file in get_files_from_cfg() {
+        let file = files::new_text_file(file).expect("File failed to open");
+        for line in file.lines() {
+            let line = line.unwrap();
+            out.write(line.as_bytes()).expect("No errors");
+            out.write(b"\n").expect("No errors");
+        }
+    }
+}
+
+// Reverse cat by iterating BufRead::lines().
+// BufRead::lines() is not a double-ended iterator, so we have to make a copy in RAM first
+// TODO: Implement a double-ended line iterator for IndexedFile and test it here
+#[allow(dead_code)]
+pub fn rev_itercat_cmd() {
+    let mut out = std::io::stdout();
+    for file in get_files_from_cfg() {
+        let file = files::new_text_file(file).expect("File failed to open");
+        let lines = file.lines().map(|x| x.unwrap()).collect::<Vec<_>>();
+        eprintln!("Read in {} lines", lines.len());
+        for line in lines.iter().rev() {
+            out.write(line.as_bytes()).expect("No errors");
+            out.write(b"\n").expect("No errors");
+        }
+    }
+}
+
+
+#[allow(dead_code)]
+pub fn bufcat_cmd() {
+    let mut out = std::io::stdout();
+    let mut buf:Vec<u8> = Vec::new();
+    buf.reserve(10 * 1024);
+    for file in get_files_from_cfg() {
+        if let Ok(mut file) = ZstdLogFile::from_path(&file.unwrap()) {
+            loop {
+                let bytes = file.read_until(b'\n', &mut buf).expect("No errors");
+                if bytes == 0 {
+                    break
+                }
+                out.write(buf.as_slice()).expect("No errors");
             }
+        }
+    }
+}
+
+
+#[test] fn test_itercat() {
+    let mut path = PathBuf::new();
+    // path.push("/home/phord/git/mine/igrok/indexed_file/resources/test/core.log-2022040423.zst");
+    path.push("/home/phord/git/mine/igrok/indexed_file/resources/test/core.log-2022040423");
+    let mut out = std::io::stdout();
+    let file = files::new_text_file(Some(path)).expect("File failed to open");
+    {
+        for line in file.lines().map(|x| x.unwrap()).collect::<Vec<_>>().iter().rev() {
+            out.write(line.as_bytes()).expect("No errors");
+            out.write(b"\n").expect("No errors");
         }
     }
 }
--- cat/src/main.rs
+++ cat/src/main.rs
@@ -3,5 +3,9 @@ mod cat;
 use cat::cat_cmd;
 
 fn main() {
-    cat_cmd();
+    cat_cmd();               // 21s
+    // cat::copycat_cmd();      // 7s
+    // cat::itercat_cmd();      // 11s
+    // cat::bufcat_cmd();       // 11s
+    // cat::rev_itercat_cmd();     // 16s
 }
