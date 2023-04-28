67c3db3f8656229e0776c3c042cf9a2c10fda372
Phil Hord <phord@purestorage.com>
2023-04-27 17:39:57 -0700

    Add conversions for LogFile -> LogSource
    I introduced LogBase to differentiate that it doesn't apply also to LogSource
    which is a LogFile itself.  This seems ugly; but it works.  :shrug:

--- indexed_file/src/files/log_file.rs
+++ indexed_file/src/files/log_file.rs
@@ -6,6 +6,7 @@ use std::io::Seek;
 use std::io::SeekFrom;
 use std::path::PathBuf;
 
+use crate::files::CursorLogFile;
 use crate::files::MockLogFile;
 use crate::files::TextLogFile;
 use crate::files::TextLogStream;
@@ -13,6 +14,26 @@ use crate::files::ZstdLogFile;
 
 
 pub type LogSource = Box<dyn LogFile>;
+
+impl<LOG: LogBase + 'static> From<LOG> for LogSource {
+    fn from(file: LOG) -> Self {
+        Box::new(file) as LogSource
+    }
+}
+
+pub trait LogBase: LogFile {
+    fn to_src(self) -> LogSource
+    where Self: Sized + 'static {
+        LogSource::from(self)
+    }
+}
+
+// All of these can be promoted to LogSource
+impl LogBase for CursorLogFile {}
+impl LogBase for MockLogFile {}
+impl LogBase for TextLogFile {}
+impl LogBase for TextLogStream {}
+impl LogBase for ZstdLogFile {}
 
 pub trait LogFile: LogFileUtil + BufRead + Seek {
 
@@ -37,22 +58,22 @@ pub fn new_text_file(input_file: Option<PathBuf>) -> std::io::Result<LogSource> 
             if let Ok(file) = ZstdLogFile::from_path(&input_file) {
                 // FIXME: If the first magic number succeeded but some later error occurred during scan, treat the
                 //        file as a compressed file anyway.
-                Ok(Box::new(file))
+                Ok(file.to_src())
             } else {
                 let file = File::open(&input_file).unwrap();
                 let file = TextLogFile::new(file);
-                Ok(Box::new(file))
+                Ok(file.to_src())
             }
         } else {
             // Must be a stream.  We can't seek in streams.
             let mut file = File::open(&input_file)?;
             assert!(file.seek(SeekFrom::Start(0)).is_err());
             let file = TextLogStream::new(Some(input_file))?;
-            Ok(Box::new(file))
+            Ok(file.to_src())
         }
     } else {
         let file = TextLogStream::new(None)?;
-        Ok(Box::new(file))
+        Ok(file.to_src())
     }
 }
 
