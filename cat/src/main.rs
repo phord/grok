a7cf776b68a1c7c59324f392db63e0fde4ae0351
Phil Hord <phord@purestorage.com>
2024-10-14 11:34:39 -0700

    Clean up comment from prev-fixed bug

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -1,11 +1,5 @@
 use tools::cat::*;
 
-/** FIXME:  First line after delay goes missing
-    $ cargo run --release --bin cat <(seq 2; sleep 2 ; seq 2)
-    1
-    2
-    2
- */
 fn main() {
     flexi_logger::Logger::try_with_env_or_str("trace").unwrap().start().unwrap();
     cat_cmd();               // 21s
